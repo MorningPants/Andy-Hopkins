d737134c58e42c79d39f5d6ddb1fe34487f56f28
MorningPants <77476821+MorningPants@users.noreply.github.com>
2022-02-21 13:39:34 -0800

    gh pages config 3

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess(),
+
 	kit: {
-		adapter: adapter()
+		target: '#svelte',
+		adapter: adapter({
+			pages: 'build',  // path to public directory
+			assets: 'build',  // path to public directory
+			fallback: null
+		})
 	}
 };
 
