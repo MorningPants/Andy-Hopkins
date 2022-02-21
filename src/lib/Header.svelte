6bdbb4df89696fa35f58d206f992d242a056f7fa
MorningPants <77476821+MorningPants@users.noreply.github.com>
2022-02-21 13:23:04 -0800

    Initial Upload

---
+++ src/lib/Header.svelte
@@ -0,0 +1,18 @@
+<div>
+	<img class="rose" src="/static/Rose_Icon.png" alt="Logo" height="100" />
+	<img class="title" src="/static/Andy_Hopkins.png" alt="Andy Hopkins" height="100" />
+</div>
+
+<style>
+div{
+    justify-content: center;
+    display: flex;
+    background: linear-gradient( #c7caed, rgb(201, 201, 204));
+}
+.rose{
+    mix-blend-mode: multiply;
+}
+.title{
+    margin-top: 16px;
+}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,37 @@
+<script>
+	import Card from '../components/Card.svelte';
+</script>
+<h1>Web Developer</h1>
+<div class="flex">
+	<img src="/static/hand-coding.svg" alt="Hand Coding" width="40%" />
+	<div>
+		<h2>Some of my work:</h2>
+		<div class="flex wrap">
+			<Card link={"https://codepen.io/MorningPants"} src={'/static/Codepen.png'} alt={'Codepen'} />
+			<Card link={"https://skyschool.arizona.edu/"} src={'/static/SkySchool.png'} alt={'Arizona Sky School'} />
+			<Card link={"https://thrivefamilymassage.com"} src={'/static/Thrive.png'} alt={'Thrive Family Massage'} />
+		</div>
+	</div>
+</div>
+
+<style>
+	.flex {
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+	}
+	.wrap{
+		flex-wrap: wrap-reverse;
+		gap: 10px;
+		max-width: 40vw;
+	}
+	h1, h2{
+		text-align: center;
+		font-family: "Antic Slab", serif;
+		font-weight: bold;
+	}
+	h2{
+		text-decoration: underline;
+		margin: 10px;
+	}
+</style>
